523c8292faa7eb832024bbc0ef1f1f8aca878673
BrzzrB <valikivasyshyn@gmail.com>
2021-09-19 01:16:08 +0300

    first commit

---
+++ src/main/java/com/example/brzz/BrzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.brzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrzzApplication.class, args);
+    }
+
+}
