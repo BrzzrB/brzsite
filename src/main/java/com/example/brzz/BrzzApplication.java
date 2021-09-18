3837072277b8e7014c491328e3836cefb2187690
BrzzrB <valikivasyshyn@gmail.com>
2021-09-19 01:20:14 +0300

    first commit

--- src/main/java/com/example/brzz/BrzzApplication.java
+++ src/main/java/com/example/brzz/BrzzApplication.java
@@ -2,12 +2,21 @@ package com.example.brzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class BrzzApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BrzzApplication.class, args);
     }
-
 }
