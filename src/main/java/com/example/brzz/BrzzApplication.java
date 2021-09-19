9ebef612e45dd0cec2df0303222153b295a36ec1
BrzzrB <valikivasyshyn@gmail.com>
2021-09-19 19:59:45 +0300

    make it better

--- src/main/java/com/example/brzz/BrzzApplication.java
+++ src/main/java/com/example/brzz/BrzzApplication.java
@@ -2,6 +2,7 @@ package com.example.brzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +14,7 @@ public class BrzzApplication {
     @RequestMapping("/index")
     @ResponseBody
     String home() {
-        return "index.html";
+        return "index";
     }
 
     public static void main(String[] args) {
